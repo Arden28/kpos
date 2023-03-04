df0522532010bb3a5a34ab1f9178a62b8b2fbad7
Arden28 <laudbouetoumoussa@gmail.com>
2023-03-04 15:52:31 +0100

    Set email 3

--- resources/views/email/users/new-kover.blade.php
+++ resources/views/email/users/new-kover.blade.php
@@ -4,7 +4,7 @@
 Salut {{ $user->name }}, nous sommes content de vous avoir à bord.
 Nous avons créé le compte de votre entreprise {{ $company->company_name }}.
 La prochaine étape est de vous connecter à votre compte et configurer les détails sur votre entreprise "{{ $company->company_name }}"
-Votre mot de passe temporaire est : 12345678.
+Votre mot de passe temporaire est : 12345678. Veuillez le changer au plus vite.
 <x-mail::button :url="{{ route('login') }}">
     Commencer maintenant
 </x-mail::button>
