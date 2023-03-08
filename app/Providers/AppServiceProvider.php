4cdea88ae20871d3c52fc95b6517f02ca19a6a93
Arden28 <laudbouetoumoussa@gmail.com>
2023-03-08 12:15:50 +0100

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use App\Models\User;
+use Illuminate\Support\Facades\URL;
 use Spatie\Onboard\Facades\Onboard;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,26 +30,11 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Model::preventLazyLoading(!app()->isProduction());
-
-
-	//     Onboard::addStep('Complétez votre Profile')
-    //     ->link(route('profile'))
-    //     ->cta('Complete')
-    //     /**
-    //      * The completeIf will pass the class that you've added the
-    //      * interface & trait to. You can use Laravel's dependency
-    //      * injection here to inject anything else as well.
-    //      */
-    //     ->completeIf(function (User $model) {
-    //         return $model->profile->isComplete();
-    //     });
-
-    //     Onboard::addStep('Complétez le profil de votre Enterprise')
-    //         ->link(route('settings.index'))
-    //         ->cta('Complete Copany')
-    //         ->completeIf(function (User $model) {
-    //             return $model->posts->count() > 0;
-    //         });
+        
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+        
     }
 
 }
