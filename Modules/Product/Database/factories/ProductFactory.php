29c6624f722ad633160340d9aae753c049086b6a
Arden28 <laudbouetoumoussa@gmail.com>
2023-04-01 12:20:04 +0200

    h

--- Modules/Product/Database/factories/ProductFactory.php
+++ Modules/Product/Database/factories/ProductFactory.php
@@ -29,13 +29,13 @@ class ProductFactory extends Factory
             'category_id' => function () {
                 return Category::inRandomOrder()->first()->id;
             },
-            'product_name' => $this->faker->word(3),
+            'product_name' => $this->faker->words(3),
             'product_code' => $this->faker->unique()->numerify('#############'),
             'product_barcode_symbology' => $this->faker->randomElement(['UPC-A', 'EAN-13', 'Code 128']),
             'product_quantity' => $this->faker->numberBetween(50, 100),
             'product_cost' => $this->faker->numberBetween(10, 50),
             'product_price' => $this->faker->numberBetween(50, 100),
-            'product_unit' => $this->faker->randomElement(['kg', 'g', 'L', 'ml']),
+            'product_unit' => $this->faker->randomElement(['bouteilles(s)', 't-shirt(s)', 'sac(s)', 'ml']),
             'product_stock_alert' => $this->faker->numberBetween(10, 20),
             'product_order_tax' => $this->faker->numberBetween(1, 5),
             'product_tax_type' => $this->faker->randomElement([null, 1, 2, 3]),
