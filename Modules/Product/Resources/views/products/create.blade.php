e665198ef486190ce94627b1693de50fa65120fd
Arden28 <laudbouetoumoussa@gmail.com>
2023-05-09 00:37:00 +0200

    try to solve input issue 2

--- Modules/Product/Resources/views/products/create.blade.php
+++ Modules/Product/Resources/views/products/create.blade.php
@@ -106,7 +106,7 @@
                                     <div class="col-lg-6">
                                       <div class="mb-3">
                                         <label for="product_cost">{{ __('Prix d\'achat / Production') }} <span class="text-danger">*</span></label>
-                                        <input step="0.01" min="0" max="9999.99"  id="product_cost" type="text" class="form-control" name="product_cost" required value="{{ old('product_cost') }}">
+                                        <input step="0.01" min="0,00" max="9999.99"  id="product_cost" type="text" class="form-control" name="product_cost" required value="{{ old('product_cost') }}">
                                       </div>
                                     </div>
                                     <div class="col-lg-6">
