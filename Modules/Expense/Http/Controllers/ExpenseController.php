7579d2b1a08899fc805674e3b2d38d6586c2b7ed
Arden28 <laudbouetoumoussa@gmail.com>
2023-05-23 01:39:54 +0200

    expenses 6

--- Modules/Expense/Http/Controllers/ExpenseController.php
+++ Modules/Expense/Http/Controllers/ExpenseController.php
@@ -42,8 +42,8 @@ class ExpenseController extends Controller
             'details' => 'nullable|string|max:1000'
         ]);
 
-        $category = ExpenseCategory::find($request->category_id)->first();
-        $account = Account::find($category->account_id)->first();
+        // $category = ExpenseCategory::find($request->category_id)->first();
+        // $account = Account::find($category->account_id)->first();
 
         Expense::create([
             'company_id' => Auth::user()->currentCompany->id,
@@ -71,7 +71,7 @@ class ExpenseController extends Controller
 
 
         $category = ExpenseCategory::find($request->category_id)->first();
-        $account = Account::find($request->account_id);
+        $account = Account::find($category->account_id)->first();
 
         $user = Auth::user()->id;
         $company = Auth::user()->currentCompany->id;
