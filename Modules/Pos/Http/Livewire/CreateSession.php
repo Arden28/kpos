e94d67f4096b7a60380bb6be46ba79c36b8075d7
Arden28 <laudbouetoumoussa@gmail.com>
2023-07-06 11:32:48 +0200

    pos 4

--- Modules/Pos/Http/Livewire/CreateSession.php
+++ Modules/Pos/Http/Livewire/CreateSession.php
@@ -82,14 +82,16 @@ class CreateSession extends Component
                         $physical->is_active = 1;
                         $physical->save();
 
-                        // Update user current pos id
-                        $user = User::find(Auth::user()->id)->first();
-                        $user->current_pos_id = $physical->id;
-                        $user->save();
 
                         session(['pos_session' => true]);
                         session(['pos_session_id' => $pos_session->id]);
                         session(['pos_id' => $this->pos_id]);
+
+                        // Update user current pos id
+                        $user = User::find(Auth::user()->id)->first();
+                        // $user->current_pos_id = $physical->id;
+                        $user->current_pos_id = session('pos_id');
+                        $user->save();
 
                         return redirect()->route('app.pos.index');
 
